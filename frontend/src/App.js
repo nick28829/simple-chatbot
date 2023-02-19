c6ffbb7a94c327ad7a90dc21ef27fbb4017ceb95
Nicklas Bocksberger <nicklas.bocksberger@gmail.com>
2023-02-19 16:43:27 +0100

    add UI improvements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
   function handleSubmit(event) {
     event.preventDefault();
     fetch(
-      'http://127.0.0.1:8080/', 
+      '/api', 
       {
         method: 'POST',
         body: JSON.stringify(
@@ -51,7 +51,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-      'http://127.0.0.1:8080/api', 
+      '/api', 
       {
         method: 'POST',
         body: JSON.stringify(
@@ -64,7 +64,6 @@ function App() {
       }
     ).then(response => response.json()
     ).then(data => {
-      console.log(data)
       if (data.context) setContext(data.context);
       if (data.message) setMessages(prevMessages => [
           ...prevMessages,
@@ -79,8 +78,9 @@ function App() {
 
   const messageElements = messages.map(message => {
     return (
-      <div className={message.sent ? 'message message-sent' : 'message message-received'}>
-        <p>{message.text}</p>
+      <div className={message.sent ? 'message message-sent' : 'message message-received'}
+        dangerouslySetInnerHTML={{ __html: `<p>${message.text}</p>`}
+      }>
       </div>
     )
   })
@@ -106,36 +106,37 @@ function App() {
           </h2>
           <p>
             This project is the result of a rainy Sunday. It is a very simple chatbot. The base is a YAML-file, where all intents
-            are listed. The correct intent is found by comparing the w2v-representation of the input with some example sentences at each intent.
+            are listed. The correct intent is found by comparing the w2v-representation of the input with some example sentences for each intent.
             If an intent needs arguments, e.g. a company name, the YAML-file provides a list of possible values. The bot determines the 
-            correct value by using the Levenshtein-distance. If the arument is not found, follow-up questions for missing arguments are also
+            correct value by using the Levenshtein-distance. If the argument is not found, follow-up questions for missing arguments are also
             provided in the configuration.
           </p>
           <h2>
             About Me:
           </h2>
           <p>
-            I am Nicklas Bocksberger, a Data Scientist and Software Engineer based in Munich. Currently, I'm employed at the Principia Mentis GmbH, 
-            where I mainly implement data-driven features as well as some backend development. Regarding my academic career I'm doing my 
-            Bachelor's at the University of Applied Sciences Munich with the major Data Science &amp; Scientific Computing. I'm interested in Natural
-            Language Processing and I'm hoping to be able to further specialize on this topic in the future.
+            I am Nicklas Bocksberger, a Data Scientist based in Munich. Currently, I'm employed at the Principia Mentis GmbH. Regarding my academic
+            career I'm doing my Bachelor's at the University of Applied Sciences Munich with the major Data Science &amp; Scientific Computing.
+            I'm interested in Natural Language Processing and I'm hoping to be able to further specialize on this topic in the future. If you are
+            interested in what I do you might consider following me on <a href='https://medium.com/@nicklas_bocksberger'>Medium</a> or send me a
+            connection request on <a href='https://www.linkedin.com/in/nicklas-bocksberger-8b23b8154/'>LinkedIn</a>.
           </p>
           <div className='spacing'/>
       </body>
       <footer>
         <div className='react-link'>
-          powered by <a href='https://reactjs.org/'>React</a>
+          built using <a href='https://reactjs.org/'>React</a>
         </div>
         <div className='footer-links'>
           <ul>
             <li>
-              <a href='#'>GitHub Repository</a>
+              <a href='https://github.com/nick28829/simple-chatbot'>GitHub Repository</a>
             </li>
             <li>
-              <a href='#'>My CV as a PDF</a>
+              <a href='https://nicklas-bocksberger.com/cv'>My CV as a PDF</a>
             </li>
             <li>
-              <a href='#'>My Website</a>
+              <a href='https://nicklas-bocksberger.com'>My Website</a>
             </li>
           </ul>
         </div>
